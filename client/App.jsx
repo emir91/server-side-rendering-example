8a067f5ebebd017dc64abc60e5272a1bb2a84f92
emir.vranac <vranacemir@gmail.com>
2021-07-06 14:18:52 +0200

    Add functionality to the app

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const App = () => {
+const App = ({questions, answers}) => {
     return (
         <div>
-          React Component  
+            <h1>Q&A Tool</h1>
+          {questions.map(({questionId, content}) => (
+              <div key={questionId}>
+                  <h3>{content}</h3>
+                  <div>
+                    {answers
+                        .filter(answer => answer.questionId === questionId)
+                        .map(({answerId, upvote, content}) => (
+                            <div key={answerId}>
+                                 <span>{content} - {upvote}</span>
+                            </div>
+                         ))}
+                  </div>
+              </div>
+              
+              
+          ))} 
         </div>
     );
 };
