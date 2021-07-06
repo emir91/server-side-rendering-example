7f24aa98a1578238e0378ff198889e274b39a223
emir.vranac <vranacemir@gmail.com>
2021-07-06 10:30:44 +0200

    Basic server setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res) => {
+    res.send('<h1>HELLO WORLD</h1>')
+});
+
+app.listen(3000);
+
+console.log('Server is listening.')
